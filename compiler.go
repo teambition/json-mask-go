7f8887373502006b4967f1168b65f044598ddab3
David Cai <davidcai1993@yahoo.com>
2017-10-12 15:49:50 +0800

    feat: implement scan()

--- compiler.go
+++ compiler.go
@@ -18,18 +18,56 @@ const (
 	typeArray
 )
 
+const keyAny = "*"
+
 // Node represents a grammar node.
 type Node struct {
-	key        string
-	typ        valueType
-	properties *Node
+	key   string
+	typ   valueType
+	props []Node
+}
+
+type token struct {
+	tag   string
+	value string
 }
 
 // Compile compiles the given mask text to the json mask nodes.
-func Compile(text string) (*Node, error) {
+func Compile(text string) ([]Node, error) {
 	if text == "" {
 		return nil, errors.WithStack(ErrEmptyString)
 	}
 
 	return nil, nil
+}
+
+func scan(text []rune) []token {
+	var (
+		tokens = []token{}
+		name   = ""
+	)
+
+	var maybePush = func() {
+		if name == "" {
+			return
+		}
+		tokens = append(tokens, token{tag: "_n", value: name})
+		name = ""
+	}
+
+	for i := 0; i < len(text); i++ {
+		ch := string(text[i])
+
+		switch ch {
+		case ",", "/", "(", ")":
+			maybePush()
+			tokens = append(tokens, token{tag: ch})
+		default:
+			name += ch
+		}
+	}
+
+	maybePush()
+
+	return tokens
 }
--- compiler_test.go
+++ compiler_test.go
@@ -6,6 +6,38 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+var tests = map[string][]Node{
+	"a": []Node{
+		Node{key: "a", typ: typeObject},
+	},
+	"a,b,c": []Node{
+		Node{key: "a", typ: typeObject},
+		Node{key: "b", typ: typeObject},
+		Node{key: "c", typ: typeObject},
+	},
+	"a/*/c": []Node{
+		Node{key: "a", typ: typeObject, props: []Node{
+			Node{
+				key:   keyAny,
+				typ:   typeObject,
+				props: []Node{Node{key: "c", typ: typeObject}},
+			},
+		}},
+	},
+	"a,b(d/*/g,b),c": []Node{
+		Node{key: "a", typ: typeObject},
+		Node{key: "b", typ: typeArray, props: []Node{
+			Node{key: "d", typ: typeObject, props: []Node{
+				Node{key: keyAny, typ: typeObject, props: []Node{
+					Node{key: "g", typ: typeObject},
+				}},
+			}},
+			Node{key: "b", typ: typeObject},
+		}},
+		Node{key: "c", typ: typeObject},
+	},
+}
+
 type CompilerSuite struct {
 	suite.Suite
 }
@@ -13,6 +45,35 @@ type CompilerSuite struct {
 func (s *CompilerSuite) TestCompileEmptyString() {
 	_, err := Compile("")
 	s.Error(err, ErrEmptyString)
+}
+
+func (s *CompilerSuite) TestScanOneProp() {
+	tokens := scan([]rune("a"))
+
+	s.Equal(len(tokens), 1)
+	s.Equal(tokens[0].tag, "_n")
+	s.Equal(tokens[0].value, "a")
+}
+
+func (s *CompilerSuite) TestScanThreeProps() {
+	tokens := scan([]rune("a,b,c"))
+
+	s.Equal(len(tokens), 5)
+	s.Equal(tokens[0].tag, "_n")
+	s.Equal(tokens[0].value, "a")
+	s.Equal(tokens[1].tag, ",")
+	s.Equal(tokens[1].value, "")
+	s.Equal(tokens[2].tag, "_n")
+	s.Equal(tokens[2].value, "b")
+	s.Equal(tokens[3].tag, ",")
+	s.Equal(tokens[3].value, "")
+	s.Equal(tokens[4].tag, "_n")
+	s.Equal(tokens[4].value, "c")
+}
+
+func (s *CompilerSuite) TestScanMoreProps() {
+	tokens := scan([]rune("a,b(d/*/g,b),c"))
+	s.Equal(len(tokens), 14)
 }
 
 func TestCompiler(t *testing.T) {
