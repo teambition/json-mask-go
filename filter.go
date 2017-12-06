724164340fd4f23df6a915ef14f76e66b9228d7f
David Cai <davidcai1993@yahoo.com>
2017-12-06 16:21:43 +0800

    refactor: only handle props on the first level

--- filter.go
+++ filter.go
@@ -3,7 +3,7 @@ package jsonmask
 import (
 	"reflect"
 
-	"github.com/fatih/structtag"
+	"gopkg.in/oleiade/reflections.v1"
 )
 
 type arrayWrapper struct {
@@ -11,101 +11,45 @@ type arrayWrapper struct {
 }
 
 func filter(obj interface{}, mask nodeMap) (interface{}, error) {
-	var (
-		newStructFileds []reflect.StructField
-		err             error
-	)
-
 	switch reflect.TypeOf(obj).Kind() {
 	case reflect.Slice, reflect.Array:
-		newStructFileds, err = filterProps(
-			arrayWrapper{K: obj},
-			nodeMap{"k": node{typ: typeArray, props: mask}},
-		)
+		filtered := make([]map[string]interface{}, 0)
 
-		if err != nil {
-			return nil, err
+		for i := 0; i < reflect.ValueOf(obj).Len(); i++ {
+			val, err := filterProps(reflect.ValueOf(obj).Index(i), mask)
+			if err != nil {
+				return nil, err
+			}
+			filtered = append(filtered, val)
 		}
+
+		return filtered, nil
 	default:
-		newStructFileds, err = filterProps(obj, mask)
-
+		filtered, err := filterProps(obj, mask)
 		if err != nil {
 			return nil, err
 		}
-	}
 
-	return reflect.ValueOf(obj).Convert(reflect.StructOf(newStructFileds)).Interface(), nil
+		return filtered, nil
+	}
 }
 
-func filterProps(obj interface{}, mask nodeMap) ([]reflect.StructField, error) {
-	objType := reflect.TypeOf(obj)
-	newFields := make([]reflect.StructField, objType.NumField())
+func filterProps(obj interface{}, mask nodeMap) (map[string]interface{}, error) {
+	filteredMap := make(map[string]interface{})
 
-	for key, node := range mask {
-		field, pos, ok := getFiledByJSONKey(obj, key)
+	for key := range mask {
+		field, _, ok := getFiledByJSONKey(obj, key)
 		if !ok {
 			continue
 		}
 
-		if node.props != nil && len(node.props) != 0 {
-			switch field.Type.Kind() {
-			case reflect.Slice, reflect.Array:
-				sliceValue := reflect.ValueOf(obj).FieldByName(field.Name)
-
-				if sliceValue.Len() == 0 {
-					continue
-				}
-
-				subFields, err := filterProps(sliceValue.Index(0).Interface(), node.props)
-				if err != nil {
-					return nil, err
-				}
-
-				subStruct := reflect.StructOf(subFields)
-
-				subSliceStruct := reflect.SliceOf(subStruct)
-
-				field.Type = reflect.SliceOf(subSliceStruct)
-			default:
-				subFields, err := filterProps(reflect.ValueOf(obj).FieldByName(field.Name).Interface(), node.props)
-				if err != nil {
-					return nil, err
-				}
-
-				field.Type = reflect.StructOf(subFields)
-			}
-		}
-
-		newFields[pos] = *field
-	}
-
-	for i := 0; i < objType.NumField(); i++ {
-		field := objType.Field(i)
-
-		if newFields[i].Name != "" {
-			continue
-		}
-
-		structTags, err := structtag.Parse(string(field.Tag))
+		val, err := reflections.GetField(obj, field.Name)
 		if err != nil {
 			return nil, err
 		}
 
-		jsonTag, err := structTags.Get("json")
-		if err != nil {
-			continue // "json" tag does not exist
-		}
-
-		jsonTag.Name = "-"
-
-		if err := structTags.Set(jsonTag); err != nil {
-			return nil, err
-		}
-
-		field.Tag = reflect.StructTag(structTags.String())
-
-		newFields[i] = field
+		filteredMap[key] = val
 	}
 
-	return newFields, nil
+	return filteredMap, nil
 }
