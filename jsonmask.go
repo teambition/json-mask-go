b7e9547d461965cd35e951299568bb40ef05d06b
David Cai <davidcai1993@yahoo.com>
2018-01-29 11:36:38 +0800

    feat: handle panic in Mask()

--- jsonmask.go
+++ jsonmask.go
@@ -1,16 +1,35 @@
 package jsonmask
 
+import (
+	"errors"
+)
+
 // Mask selects the specific parts of an object, according to the "mask".
-func Mask(obj interface{}, mask string) (interface{}, error) {
+func Mask(obj interface{}, mask string) (res interface{}, err error) {
+	defer func() {
+		if r := recover(); err != nil {
+			res = obj
+
+			switch r.(type) {
+			case error:
+				err = r.(error)
+			case string:
+				err = errors.New(r.(string))
+			default:
+				err = errors.New("json mask panic")
+			}
+		}
+	}()
+
 	compiledMask, err := compile(mask)
 	if err != nil {
 		return nil, err
 	}
 
-	filteredObj, err := filter(obj, compiledMask)
+	res, err = filter(obj, compiledMask)
 	if err != nil {
 		return nil, err
 	}
 
-	return filteredObj, nil
+	return res, nil
 }
